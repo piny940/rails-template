fdbbefe3863ffb5908365beaf1a984b9fb39cc0e
Mikan <83708535+piny940@users.noreply.github.com>
2023-05-28 19:59:10 +0900

    CIでテスト・フォーマットチェックを行えるようにする (#9)
    * ciのファイルを作成
    
    * rubocop
    
    * テスト環境でのDBの設定を記述

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins [
-      'http://localhost:3001', # frontend host
+      'http://localhost:3001' # frontend host
     ]
     resource '*',
              methods: %i[get post put patch delete options head],
