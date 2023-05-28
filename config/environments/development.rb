83b2b124188e2296a2b93b1403b905d7226a2e1d
Mikan <83708535+piny940@users.noreply.github.com>
2023-05-28 22:01:52 +0900

    GCSの設定を追加 (#14)
    * gcsの設定を追加
    
    * GCSの設定を追加
    
    * .env.sampleを追加
    
    * CIを修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,4 +71,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.active_storage.service = :gcs
 end
