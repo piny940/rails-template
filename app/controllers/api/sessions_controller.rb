3c285b533a12f29b8b5ac2d49ed7197caa12b4ce
piny940 <an.s.shoma@gmail.com>
2023-06-11 16:28:35 +0900

    ログイン・ログアウトのテストを作成

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,19 +2,17 @@ class Api::SessionsController < Api::Base
   def create
     user = User.find_by(email: params[:email])
     if user&.valid_password?(params[:password])
-      user.remember_me = params[:remember_me]
       sign_in user
-      render json: { message: 'ログインしました。',
-                     user:
-                    }, status: :ok
+      render json: { message: 'ログインしました',
+                     user: }, status: :ok
     else
-      render json: { message: 'メールアドレスまたはパスワードが違います。' }, status: :bad_request
+      render json: { message: 'メールアドレスまたはパスワードが違います' }, status: :bad_request
     end
   end
 
   def destroy
     sign_out current_user
-    render json: { message: 'ログアウトしました。', data: {
+    render json: { message: 'ログアウトしました', data: {
       user: nil
     } }, status: :ok
   end
