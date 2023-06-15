f3fbb9cd9e3e2b708ca6026d5e5be1cdeddbc947
piny940 <an.s.shoma@gmail.com>
2023-06-15 11:52:23 +0900

    capstinoの設定ファイルを作成

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,36 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.17.3"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "rails_template"
+set :repo_url, "git@github.com:piny940/rails-template.git"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# sharedディレクトリに入れるファイルを指定
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+# SSH接続設定
+set :ssh_options, {
+  auth_methods: ['publickey'], 
+  keys: ['~/.ssh/kagoya.pem'] 
+}
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+# 保存しておく世代の設定
+set :keep_releases, 5
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+# rbenvの設定
+set :rbenv_type, :user
+set :rbenv_ruby, '3.1.2'
 
-# Default value for :pty is false
-# set :pty, true
+# ここからUnicornの設定
+# Unicornのプロセスの指定
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+# Unicornの設定ファイルの指定
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+# Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
